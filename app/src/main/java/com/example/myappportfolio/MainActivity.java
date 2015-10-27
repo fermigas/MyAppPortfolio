6a094909c40d1820180aa9cee403fdb6d27a6a89
Jon Teets <jonteets@SILVER-SURFER.local>
2015-10-27 18:44:56 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myappportfolio/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.myappportfolio;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void capstone(View view){
+        CharSequence text = "I'm Capstone!";
+        toastMaster(text);
+    }
+
+    public void spotify(View view){
+        CharSequence text = "Spotify Streamer, Dude!";
+        toastMaster(text);
+    }
+
+    public void scoresApp(View view){
+        CharSequence text = "Scores App.";
+        toastMaster(text);
+    }
+
+    public void libraryApp(View view){
+        CharSequence text = "Libarary App";
+        toastMaster(text);
+    }
+
+    public void buildItBigger(View view){
+        CharSequence text = "Build It Bigger";
+        toastMaster(text);
+    }
+
+    public void xyzReader(View view){
+        CharSequence text = "xyz Reader";
+        toastMaster(text);
+    }
+
+    private void toastMaster(CharSequence text){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+}
